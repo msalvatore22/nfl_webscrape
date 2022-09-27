248622ffbb9acebc479bdc69a07b91f813140991
Michael Salvatore <msalvatore22@gmail.com>
2022-09-27 17:29:25 -0400

    Switched to plotly for experimenting with data visualizations using python

---
+++ app.py
@@ -0,0 +1,36 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+from espn_team_player_stats import espn_team_player_stats
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+# df = pd.DataFrame({
+#     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#     "Amount": [4, 1, 2, 2, 4, 5],
+#     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+# })
+
+all_stats = espn_team_player_stats()
+
+passing_stats = all_stats["Passing"]
+
+fig = px.bar(passing_stats[['YDS']], y="YDS", color="YDS", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
